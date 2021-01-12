4d4a4f5b206eead05605943f65ed5304c28b7d5a
fordsaranpong <forddbkzero@gmail.com>
2021-01-12 15:28:32 +0700

    lab10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+srand(time(0));
+int ran=rand()%9;
+string x[]={"A","B+","B","C+","C","D+","D","F","W"};
+std::cout<<"Press Enter 3 times to reveal your future.";
+cin.get();cin.get();cin.get();
+cout<<"You will get "<<x[ran]<<" in this 261102.";
+return 0;
+}
